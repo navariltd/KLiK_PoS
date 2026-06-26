b5b225b4cbaf59c4faef1b922991e3eaabf97ce1
KushX <kushal@cecypo.com>
2026-06-26 13:04:25 +0300

    feat: walk-in party field installer + POS Profile button

--- klik_pos/public/js/pos_profile.js
+++ klik_pos/public/js/pos_profile.js
@@ -3,5 +3,29 @@ frappe.ui.form.on('POS Profile', {
         if (!frm.doc.custom_default_view) {
             frm.set_value('custom_default_view', frm.doc.hide_images ? 'List View' : 'Grid View');
         }
+    },
+
+    refresh(frm) {
+        frm.add_custom_button(__('Install Walk-in Party Fields'), () => {
+            frappe.confirm(
+                __('Add Walk-in Name / Tax ID / Phone fields to Quotation, Sales Order, Delivery Note and Sales Invoice?'),
+                () => {
+                    frappe.call({
+                        method: 'klik_pos.setup.walkin_fields.install_walkin_party_fields',
+                        freeze: true,
+                        freeze_message: __('Installing walk-in fields...'),
+                        callback: (r) => {
+                            if (!r.exc) {
+                                frappe.show_alert({
+                                    message: __('Walk-in fields installed on: {0}',
+                                        [(r.message.installed_on || []).join(', ')]),
+                                    indicator: 'green',
+                                });
+                            }
+                        },
+                    });
+                }
+            );
+        }, __('Setup'));
     }
 });
